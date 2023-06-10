4e64efc1ef5cba5526089d0d5dcf0dcc168a4a9d
Marcelo F. Cândido <marcelo.candido@rarolabs.com.br>
2023-06-10 15:30:28 -0300

    wip: creating types for requests and responses

---
+++ primes/src/common/interfaces/index.ts
@@ -0,0 +1,9 @@
+import HealthStatus from './HealthStatus';
+import PrimeOrder from './PrimeOrder';
+import PrimeCheck from './PrimeCheck';
+
+export {
+  HealthStatus,
+  PrimeOrder,
+  PrimeCheck,
+};
